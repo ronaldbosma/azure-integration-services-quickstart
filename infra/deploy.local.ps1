45ba517964f11f2a5e6568e37fe8a759fc442854
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-04 18:02:21 +0100

    rename src folder to infra, for azd

---
+++ infra/deploy.local.ps1
@@ -0,0 +1,6 @@
+./deploy.ps1 -Environment "aisquick" `
+             -Location "swedencentral"
+
+
+# az account set --subscription "Visual Studio Enterprise"
+# az account set --subscription "Visual Studio Enterprise 2"
