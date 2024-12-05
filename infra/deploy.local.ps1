eac8ca97ff0133b16cb5dc1175d7b3b4f14e6222
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-05 17:16:16 +0100

    Remove local files

--- infra/deploy.local.ps1
+++
@@ -1,6 +0,0 @@
-./deploy.ps1 -Environment "aisquick" `
-             -Location "swedencentral"
-
-
-# az account set --subscription "Visual Studio Enterprise"
-# az account set --subscription "Visual Studio Enterprise 2"
