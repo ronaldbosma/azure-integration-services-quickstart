b5cbf58c7c0fa9bb207cdd8afaf531f0fc5e6e3c
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-22 12:04:05 +0100

    Fix logging

--- src/functionApp/Program.cs
+++ src/functionApp/Program.cs
@@ -1,14 +1,41 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
+// Register log levels and categories. Based on: https://github.com/Azure/azure-functions-dotnet-worker/issues/2531
+builder.Configuration.AddConfiguration(
+    new ConfigurationBuilder()
+        .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!) // Required on Linux
+        .AddJsonFile("appsettings.json") // First read configuration file
+        .AddEnvironmentVariables() // Then override using app settings
+        .Build()
+        .GetSection("Logging")
+    );
+
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
-    .ConfigureFunctionsApplicationInsights();
+    .ConfigureFunctionsApplicationInsights()
+    .Configure<LoggerFilterOptions>(options =>
+    {
+        // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs.
+        // Application Insights requires an explicit override.
+        // Log levels can also be configured using appsettings.json.
+        // For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+        var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
+            == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
+
+        if (toRemove is not null)
+        {
+            options.Rules.Remove(toRemove);
+        }
+    });
 
 builder.Build().Run();
