f3512d5726d104cf8034d118b01072121b532bd6
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-22 13:08:30 +0100

    Update app insights comments in program

--- src/functionApp/Program.cs
+++ src/functionApp/Program.cs
@@ -10,7 +10,7 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Register log levels and categories. Based on: https://github.com/Azure/azure-functions-dotnet-worker/issues/2531
+// Load log levels and categories from appsettings.json. Based on: https://github.com/Azure/azure-functions-dotnet-worker/issues/2531
 builder.Configuration.AddConfiguration(
     new ConfigurationBuilder()
         .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!) // Required on Linux
@@ -26,9 +26,8 @@ builder.Services
     .Configure<LoggerFilterOptions>(options =>
     {
         // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs.
-        // Application Insights requires an explicit override.
-        // Log levels can also be configured using appsettings.json.
-        // For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+        // Application Insights requires an explicit override. Log levels can also be configured using appsettings.json.
+        // For more information, see https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#managing-log-levels
         var toRemove = options.Rules.FirstOrDefault(rule => rule.ProviderName
             == "Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider");
 
