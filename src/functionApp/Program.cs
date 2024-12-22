cb6caff8fcffa25ff04d177975a0f12bac7735eb
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-22 10:52:37 +0100

    Enable app insights

--- src/functionApp/Program.cs
+++ src/functionApp/Program.cs
@@ -5,9 +5,8 @@ var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
 
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
 
 builder.Build().Run();
