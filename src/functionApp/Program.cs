baca7da490bf6633e924386a55873b28d26fec09
Ronald Bosma <ronaldbosma@outlook.com>
2024-12-22 10:56:55 +0100

    Add app insights packages and using statements to fix build

--- src/functionApp/Program.cs
+++ src/functionApp/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = FunctionsApplication.CreateBuilder(args);
