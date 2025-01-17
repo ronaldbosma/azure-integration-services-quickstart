45164266f940b355d0a9bbad2776f0705d4f0539
Ronald Bosma <ronaldbosma@outlook.com>
2025-01-17 17:25:19 +0100

    Add sample function that triggers on event hub msg

---
+++ src/functionApp/EventHubSampleFunction.cs
@@ -0,0 +1,27 @@
+using AISQuick.FunctionApp.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AISQuick.FunctionApp;
+
+public class EventHubSampleFunction
+{
+    private readonly ILogger<EventHubSampleFunction> _logger;
+
+    public EventHubSampleFunction(ILogger<EventHubSampleFunction> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function(nameof(EventHubSampleFunction))]
+    [TableOutput("aisquickSample", Connection = "StorageAccountConnection")]
+    public SampleTableEntity Run(
+        [EventHubTrigger("aisquick-sample", ConsumerGroup = "function-app", Connection = "EventHubConnection", IsBatched = false)]
+        SampleMessage sampleMessage
+    )
+    {
+        _logger.LogInformation("Received message '{message}' with ID {id}", sampleMessage.Message, sampleMessage.Id);
+
+        return new SampleTableEntity(sampleMessage.Id, sampleMessage.Message);
+    }
+}
